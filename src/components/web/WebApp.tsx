168ef02890aab28737bc1677ef5067cf56e4c96e
Raymond Jacobson <ray@audius.co>
2021-01-05 18:50:57 -0800

    Clean up

--- src/components/web/WebApp.tsx
+++ src/components/web/WebApp.tsx
@@ -29,8 +29,8 @@ import useKeyboardListeners from '../../utils/useKeyboardListeners'
 import NotificationReminder from '../notification-reminder/NotificationReminder'
 import {postMessage as postMessageUtil} from '../../utils/postMessage'
 
-const USE_LOCALHOST_APP = Config.USE_LOCALHOST_APP
-const STATIC_PORT = Config.STATIC_SERVER_PORT
+const URL_OVERRIDE = Config.URL_OVERRIDE
+const STATIC_PORT = Config.STATIC_SERVER_PORT || 3100
 export const URL_SCHEME = 'audius://'
 
 // Intercept localhost://, file:///, or audius://
@@ -435,7 +435,7 @@ const WebApp = ({
     setAtTop(navState.nativeEvent.contentOffset.y <= 1)
   }
 
-  const uri = USE_LOCALHOST_APP ? `http://localhost:${STATIC_PORT}` : url
+  const uri = URL_OVERRIDE || url
   if (!uri) return null
   return (
     <>
