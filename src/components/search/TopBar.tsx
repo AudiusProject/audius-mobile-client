fe2784b7cdaad34d1c29a1ff571f463e09042306
jowlee <joeylee0925@gmail.com>
2021-04-30 09:57:06 -0400

    Add tag search to search bar input and styles

--- src/components/search/TopBar.tsx
+++ src/components/search/TopBar.tsx
@@ -12,61 +12,70 @@ import LottieView from 'lottie-react-native'
 
 import IconCaretRight from '../../assets/images/iconCaretRight.svg'
 import IconRemove from '../../assets/images/iconRemove.svg'
-import { useColor, useSpecialColor, useTheme } from '../../utils/theme'
+import { useColor, useTheme } from '../../utils/theme'
 import { useDispatchWebAction } from '../../hooks/useWebAction'
 import { MessageType } from '../../message'
-
+import {
+  getTagSearchRoute
+} from '../../utils/routes'
 import { getSearchQuery, getSearchResultQuery } from '../../store/search/selectors'
 import { updateQuery } from '../../store/search/actions'
 import useSearchHistory from '../../store/search/hooks'
+import { usePushSearchRoute } from './utils'
 
 
 const IS_IOS = Platform.OS === 'ios'
 
 const styles = StyleSheet.create({
   topBar: {
-    height: IS_IOS ? 85 : 55,
+    height: IS_IOS ? 86 : 55,
     borderBottomWidth: 1
   },
   container: {
     position: 'absolute',
-    bottom: 5,
+    bottom: 0,
     flex: 1,
     width: '100%',
+    display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
     height: 30,
-    paddingLeft: 12,
+    paddingLeft: 0,
     paddingRight: 16,
+  },
+  caretContainer: {
+    paddingBottom: 6,
+    paddingLeft: 6,
+    paddingRight: 12
   },
   caret: {
     transform: [{ rotate: '180deg' }]
   },
   removeIcon: {
     position: 'absolute',
-    right: 20,
-    bottom: 3
+    right: 4,
+    bottom: -10,
+    padding: 16
   },
   loadingIcon: {
     position: 'absolute',
     right: 20,
-    bottom: 3,
+    bottom: 6,
     width: 24,
     height: 24
   },
   input: {
     position: 'relative',
-    marginLeft: 12,
     flex: 1,
     height: '100%',
     borderRadius: 8,
     borderWidth: 1,
     paddingLeft: 8,
     paddingRight: 26,
+    paddingTop: 0,
+    paddingBottom: 0,
+    marginBottom: 6,
     fontFamily: 'AvenirNextLTPro-Medium'
-  },
-  temp: {
-    color: 'red'
   }
 })
 
@@ -105,22 +114,31 @@ const TopBar = ({
     dispatch(updateQuery(text))
   }, [dispatch])
 
+  const pushRoute = usePushSearchRoute()
+
   const onSubmit = useCallback(({ nativeEvent: { text } }) => {
     appendSearchItem(text)
-    dispatchWeb({
-      type: MessageType.UPDATE_SEARCH_QUERY,
-      query: text
-    })
-  }, [dispatchWeb, appendSearchItem])
+    if (text.startsWith('#')) {
+      pushRoute(getTagSearchRoute(text.substring(1)), 'search')
+    } else {
+      dispatchWeb({
+        type: MessageType.UPDATE_SEARCH_QUERY,
+        query: text
+      })
+    }
+  }, [dispatchWeb, pushRoute, appendSearchItem])
 
 
   const onChangeText = useCallback((text: string) => {
     setQuery(text)
-    dispatchWeb({
-      type: MessageType.UPDATE_SEARCH_QUERY,
-      query: text,
-      isAction: true
-    })
+    if (!text.startsWith('#')) {
+      setQuery(text)
+      dispatchWeb({
+        type: MessageType.UPDATE_SEARCH_QUERY,
+        query: text,
+        isAction: true
+      })
+    }
   }, [dispatchWeb, setQuery])
 
   const clearSearch = useCallback(() => {
@@ -129,20 +147,22 @@ const TopBar = ({
   }, [textRef, setQuery])
 
   const searchResultQuery = useSelector(getSearchResultQuery)
+  const isTagSearch = query.startsWith('#')
   const hasText = query !== ''
-  const isLoading = hasText && searchResultQuery !== query
+  const isLoading = !isTagSearch && hasText && searchResultQuery !== query
   const spinnerColor = useColor('neutralLight4')
 
   return (
     <View style={topBarStyle}>
       <View style={styles.container}>
-        <TouchableOpacity activeOpacity={0.7} onPress={onClose}>
-          <IconCaretRight width={24} height={24} fill={color} style={styles.caret} />
+        <TouchableOpacity activeOpacity={0.7} onPress={onClose} style={styles.caretContainer}>
+          <IconCaretRight width={28} height={28} fill={color} style={styles.caret} />
         </TouchableOpacity>
         <TextInput
           ref={textRef}
           value={query}
           onChangeText={onChangeText}
+          underlineColorAndroid="transparent"
           style={inputStyles}
           autoCompleteType={'off'}
           autoCorrect={false}
