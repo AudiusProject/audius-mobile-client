3e57abb9b06c63ef1730173aa30f613d41d16d3e
sliptype <sliptype@gmail.com>
2021-09-10 10:09:11 -0500

    Continue splitting out handlers to match client directory structure

--- src/message/handleMessage.ts
+++ src/message/handleMessage.ts
@@ -1,30 +1,39 @@
-import { Dispatch } from 'redux'
 import { Platform } from 'react-native'
+import { Dispatch } from 'redux'
+
 import { AnalyticsMessage } from '../types/analytics'
 
-import { Message, MessageHandlers } from './types'
+import { messageHandlers as analytics } from './handlers/analytics'
+import { messageHandlers as android } from './handlers/android'
 import { messageHandlers as audio } from './handlers/audio'
 import { messageHandlers as cast } from './handlers/cast'
-import { messageHandlers as notification } from './handlers/notification'
-import { messageHandlers as search } from './handlers/search'
+import { messageHandlers as haptics } from './handlers/haptics'
 import { messageHandlers as lifecycle } from './handlers/lifecycle'
-import { messageHandlers as oauth } from './handlers/oauth'
-import { messageHandlers as android } from './handlers/android'
-import { messageHandlers as global } from './handlers/global'
 import { messageHandlers as linking } from './handlers/linking'
+import { messageHandlers as logging } from './handlers/logging'
+import { messageHandlers as notification } from './handlers/notification'
+import { messageHandlers as oauth } from './handlers/oauth'
+import { messageHandlers as search } from './handlers/search'
+import { messageHandlers as theme } from './handlers/theme'
+import { messageHandlers as version } from './handlers/version'
+import { Message, MessageHandlers } from './types'
 
 const isIos = Platform.OS === 'ios'
 
 const messageHandlers: Partial<MessageHandlers> = {
+  ...analytics,
+  ...(isIos ? {} : android),
   ...audio,
   ...cast,
-  ...notification,
-  ...search,
+  ...haptics,
   ...lifecycle,
-  ...oauth,
-  ...global,
   ...linking,
-  ...(isIos ? {} : android)
+  ...logging,
+  ...notification,
+  ...oauth,
+  ...search,
+  ...theme,
+  ...version
 }
 
 export const handleMessage = async (
--- src/message/handlers/theme.ts
+++ src/message/handlers/theme.ts
@@ -1,22 +1,15 @@
-import VersionNumber from 'react-native-version-number'
-
 import * as themeActions from '../../store/theme/actions'
-import * as haptics from '../../haptics'
+import { handleThemeChange } from '../../utils/theme'
+
 import {
   getInitialDarkModePreference,
   getPrefersDarkModeChange
 } from '../../theme'
-import { handleWebAppLog } from '../../utils/logging'
-import { handleThemeChange } from '../../utils/theme'
+import { MessageType, MessageHandlers } from '../types'
 
 let sentInitialTheme = false
 
-import { MessageType, MessageHandlers } from '../types'
-
 export const messageHandlers: Partial<MessageHandlers> = {
-  [MessageType.LOGGING]: message => {
-    handleWebAppLog(message.level, message.message)
-  },
   [MessageType.THEME_CHANGE]: (message, dispatch) => {
     dispatch(themeActions.set(message.theme))
     handleThemeChange(message.theme)
@@ -34,16 +27,5 @@ export const messageHandlers: Partial<MessageHandlers> = {
       id: message.id,
       prefersDarkMode: prefers
     })
-  },
-  [MessageType.GET_VERSION]: (message, _, postMessage) => {
-    const version = VersionNumber.appVersion
-    postMessage({
-      type: message.type,
-      id: message.id,
-      version
-    })
-  },
-  [MessageType.HAPTIC_FEEDBACK]: _ => {
-    haptics.light()
   }
 }
