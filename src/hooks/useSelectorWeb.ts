bd9edbc2d901a2adb3fc964caaa2abf30a7af8d9
sliptype <sliptype@gmail.com>
2021-09-16 14:55:45 -0500

    Map deps so that client can be imported

--- src/hooks/useSelectorWeb.ts
+++ src/hooks/useSelectorWeb.ts
@@ -1,7 +1,14 @@
-import { useSelector } from 'react-redux'
+import { isEmpty } from 'lodash'
+import { Selector, useSelector } from 'react-redux'
+
+import { AppState } from 'audius-client/src/store/types'
 
 // When mobile client is no longer dependent on the web client
 // calls to useSelectorWeb can be replaced with useSelector
-export const useSelectorWeb = selector => {
-  return useSelector((state: any) => selector(state.clientStore))
+export const useSelectorWeb = <ReturnValue>(
+  selector: Selector<AppState, ReturnValue>
+) => {
+  return useSelector((state: { clientStore: AppState }) =>
+    isEmpty(state.clientStore) ? undefined : selector(state.clientStore)
+  )
 }
