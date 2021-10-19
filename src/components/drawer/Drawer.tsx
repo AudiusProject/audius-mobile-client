f2e64cdac47a574c83faf25acfc2d6cbcd6ecf27
sliptype <sliptype@gmail.com>
2021-10-19 14:29:39 -0500

    Add Drawer component

--- src/components/drawer/Drawer.tsx
+++ src/components/drawer/Drawer.tsx
@@ -10,7 +10,8 @@ import {
   Dimensions,
   LayoutChangeEvent,
   PanResponder,
-  StyleSheet
+  StyleSheet,
+  View
 } from 'react-native'
 import { Portal } from '@gorhom/portal'
 import { SafeAreaView } from 'react-native-safe-area-context'
@@ -30,24 +31,13 @@ const styles = StyleSheet.create({
   fullDrawer: {
     top: 0,
     height: '100%'
-    // overflowX: 'hidden',
-    // overflowY: 'scroll'
   },
-
-  // fullDrawer > div {
-  //   margin-top: env(safe-area-inset-top, 0px),
-  //   margin-bottom: 10vh,
-  // }
 
   dismissContainer: {
     position: 'absolute',
     top: 16,
     left: 16
   },
-
-  // dismissContainer path {
-  //   fill: var(--neutral-light-4),
-  // },
 
   isOpen: {
     shadowOpacity: 0.25,
@@ -57,44 +47,35 @@ const styles = StyleSheet.create({
     }
   },
 
-  background: {
-    position: 'absolute',
-    right: 0,
-    left: 0,
-    top: 0,
-    bottom: 0
+  skirt: {
+    width: '100%',
+    height: 800
   }
 })
 
-const INITIAL_OFFSET = 10
 const MAX_SHADOW_OPACITY = 0.4
 const ON_MOVE_RESPONDER_DX = 20
 const ON_MOVE_RESPONDER_DY = 10
 const MOVE_CUTOFF_CLOSE = 0.8
-const MOVE_CUTOFF_OPEN = 0.2
+
+// Controls the amount of friction in swiping when overflowing up or down
+const OVERFLOW_FRICTION = 4
 
 export type DrawerProps = {
   isOpen: boolean
-  onOpen: () => void
   children: ReactNode
   onClose: () => void
   isFullscreen?: boolean
 }
 
-const Drawer = ({
-  isOpen,
-  onOpen,
-  children,
-  onClose,
-  isFullscreen
-}: DrawerProps) => {
+const Drawer = ({ isOpen, children, onClose, isFullscreen }: DrawerProps) => {
   const drawerStyle = useTheme(styles.drawer, {
     backgroundColor: 'neutralLight10'
   })
 
   const { height } = Dimensions.get('window')
   const [drawerHeight, setDrawerHeight] = useState(0)
-  const initialPosition = height + INITIAL_OFFSET
+  const initialPosition = height
   const openPosition = height - drawerHeight
 
   const translationAnim = useRef(new Animated.Value(initialPosition)).current
@@ -151,6 +132,7 @@ const Drawer = ({
     onPanResponderMove: (e, gestureState) => {
       if (isOpen) {
         if (gestureState.dy > 0) {
+          // Dragging downwards
           Animated.event(
             [
               null,
@@ -160,6 +142,17 @@ const Drawer = ({
             ],
             { useNativeDriver: false }
           )(e, { dy: openPosition + gestureState.dy })
+        } else if (gestureState.dy < 0) {
+          // Dragging upwards
+          Animated.event(
+            [
+              null,
+              {
+                dy: translationAnim
+              }
+            ],
+            { useNativeDriver: false }
+          )(e, { dy: openPosition + gestureState.dy / OVERFLOW_FRICTION })
         }
       }
     },
@@ -183,16 +176,10 @@ const Drawer = ({
   // NOTE: This is currently handled by the web app
   //   const clickOutsideRef = useClickOutside(() => close())
 
-  // TODO: border radius transition for fullscreen drawer
-
   return (
     <Portal>
       <Animated.View
         {...panResponder.panHandlers}
-        onLayout={(event: LayoutChangeEvent) => {
-          const { height } = event.nativeEvent.layout
-          setDrawerHeight(height)
-        }}
         style={[
           drawerStyle,
           ...(isFullscreen ? [styles.fullDrawer] : []),
@@ -206,54 +193,19 @@ const Drawer = ({
           }
         ]}
       >
-        <SafeAreaView edges={['bottom']}>{children}</SafeAreaView>
+        <SafeAreaView
+          edges={['bottom']}
+          onLayout={(event: LayoutChangeEvent) => {
+            const { height } = event.nativeEvent.layout
+            setDrawerHeight(height)
+          }}
+        >
+          {children}
+        </SafeAreaView>
+        <View style={styles.skirt}></View>
       </Animated.View>
     </Portal>
   )
 }
-
-const interpolateBorderRadius = (r: number) => {
-  // multiply R by some constant and then clamp so that for the majority
-  // of the transition, it stays at it's initial value
-  const r2 = Math.max(Math.min(r * 10, 40), 0)
-  return `${r2}px ${r2}px 0px 0px`
-}
-
-// const FullscreenDrawer = ({ children, isOpen, onClose }: DrawerProps) => {
-//   const drawerRef = useRef<HTMLDivElement | null>(null)
-
-//   const transitions = useTransition(isOpen, {
-//     from: {
-//       y: 1,
-//       borderRadius: 40
-//     },
-//     enter: {
-//       y: 0,
-//       borderRadius: 0
-//     },
-//     leave: {
-//       y: 1,
-//       borderRadius: 40
-//     },
-//     config: slowWobble
-//   })
-//   return (
-//     <Portal>
-//       <Animated.View
-//         ref={drawerRef}
-//         className={[styles.drawer, styles.fullDrawer]}
-//         style={{
-//           transform: y.to(y => interpY(window.innerHeight * y)),
-//           borderRadius: borderRadius.to(interpolateBorderRadius)
-//         }}
-//       >
-//         {/* <View style={styles.dismissContainer} onClick={onClose}>
-//                 <IconRemove />
-//               </View> */}
-//         {children}
-//       </Animated.View>
-//     </Portal>
-//   )
-// }
 
 export default Drawer
