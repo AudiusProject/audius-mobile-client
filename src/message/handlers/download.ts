a0ee1217ba7a88a140f3821f9b476a717763b582
Milind Shah <milinds.905@gmail.com>
2021-12-02 16:27:49 -0800

    Temp changes

--- src/message/handlers/download.ts
+++ src/message/handlers/download.ts
@@ -4,11 +4,13 @@ import { MessageType, MessageHandlers } from 'app/message/types'
 
 export const messageHandlers: Partial<MessageHandlers> = {
   [MessageType.DOWNLOAD_TRACK]: async ({ message }) => {
-    console.log('inside downloadFile handler', message.url)
+    console.log('inside downloadFile handler', message)
+    const url = message.urls[0]
+
     Share.open({
       message: message.title,
-      url: message.url,
-      saveToFiles: true
+      url: url,
+      saveToFiles: message.saveToFiles
     })
       .then(res => console.log('success: ', res))
       .catch(err => console.log('error: ', err))
