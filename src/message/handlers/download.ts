506bdd525007306f461cf67e0dc624cde65300db
Raymond Jacobson <ray@audius.co>
2021-12-03 18:00:29 -0800

    wip

--- src/message/handlers/download.ts
+++ src/message/handlers/download.ts
@@ -1,26 +1,64 @@
-import RNFetchBlob from 'rn-fetch-blob'
+import 'react-native-url-polyfill/auto'
+
+import { Platform, Share } from 'react-native'
+import RNFetchBlob, { FetchBlobResponse } from 'rn-fetch-blob'
 
 import { MessageType, MessageHandlers } from 'app/message/types'
 
 export const messageHandlers: Partial<MessageHandlers> = {
   [MessageType.DOWNLOAD_TRACK]: async ({ message }) => {
-    const fileUrl = message.urls.find(url => url !== null && url !== undefined)
-    const fileExtension = fileUrl.split('.').pop()
+    try {
+      const fileUrl = message.urls.find(
+        url => url !== null && url !== undefined
+      )
+      const fileExtension = fileUrl.split('.').pop()
+      const url = new URL(fileUrl)
+      const fileName = url.searchParams.get('filename')
 
-    // TODO (milind): Figure out why it won't save in the MusicDir
-    console.log(RNFetchBlob.fs.dirs)
-    const dirToSave = RNFetchBlob.fs.dirs.DocumentDir
+      if (Platform.OS === 'ios') {
+        // On iOS fetch & cache the track, then share with react-native's share
+        // to trigger the share sheet
 
-    RNFetchBlob.config({
-      fileCache: true,
-      path: dirToSave + '/' + message.title + '.' + fileExtension
-    })
-      .fetch('GET', fileUrl)
-      .then(res => {
-        // TODO: Show some kind of toast suggesting the file was downloaded
-        console.log('The file saved to ', res.path())
-      })
+        const dirToSave = RNFetchBlob.fs.dirs.DocumentDir
+        const res: FetchBlobResponse = await new Promise((resolve, reject) => {
+          RNFetchBlob.config({
+            fileCache: true,
+            path: dirToSave + '/' + message.title + '.' + fileExtension
+          })
+            .fetch('GET', fileUrl)
+            .then(res => resolve(res))
+            .catch(err => reject(err))
+        })
+        await Share.share({
+          message: fileName,
+          url: res.path()
+        })
+        res.flush()
+      } else {
+        // On android save to FS and trigger notification that it is saved
+        const dirToSave = RNFetchBlob.fs.dirs.DownloadDir
+
+        await new Promise((resolve, reject) => {
+          RNFetchBlob.config({
+            addAndroidDownloads: {
+              notification: true,
+              title: fileName,
+              useDownloadManager: true,
+              description: fileName,
+              path: dirToSave + '/' + message.title + '.' + fileExtension,
+              mime: 'audio/mpeg',
+              mediaScannable: true
+            }
+          })
+            .fetch('GET', fileUrl)
+            .then(res => resolve(res))
+            .catch(err => reject(err))
+        })
+        console.log('here')
+      }
+    } catch (e) {
       // TODO: How do we do error handling?
-      .catch(err => console.log('error', err))
+      console.error(e)
+    }
   }
 }
