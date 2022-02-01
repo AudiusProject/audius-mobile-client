eebd6fd54fce037fa2d9cd1ac5a09531ecd65b39
Dylan Jeffers <dylan@audius.co>
2022-02-01 10:19:38 -0800

    Replace fontWeight, add spacing

--- src/styles/makeStyles.ts
+++ src/styles/makeStyles.ts
@@ -5,21 +5,28 @@ import { StyleSheet, TextStyle, ViewStyle, ImageStyle } from 'react-native'
 import { ThemeContext } from '../components/theme/ThemeContext'
 import { ThemeColors, useThemeColors } from '../utils/theme'
 
+import { spacing } from './spacing'
 import { typography } from './typography'
 
-type Theme = { palette: ThemeColors; typography: typeof typography }
+type Theme = {
+  palette: ThemeColors
+  typography: typeof typography
+  spacing: typeof spacing
+}
 
 type NamedStyles<T> = { [P in keyof T]: ViewStyle | TextStyle | ImageStyle }
 
-type Styles<T extends NamedStyles<T>> = (theme: Theme) => T
+type Styles<T extends NamedStyles<T>> = (theme: Theme) => T | NamedStyles<T>
 
-export const makeStyles = <T extends NamedStyles<T>>(styles: Styles<T>) => {
-  const useStyles = () => {
+export const makeStyles = <T extends NamedStyles<T> | NamedStyles<any>>(
+  styles: Styles<T>
+) => {
+  const useStyles = (): T => {
     const { getTheme } = useContext(ThemeContext)
     const themeMode = getTheme()
     const themeColors = useThemeColors()
     const palette = { mode: themeMode, ...themeColors }
-    const theme: Theme = { palette, typography }
+    const theme: Theme = { palette, typography, spacing }
     const namedStyles = styles(theme)
     return StyleSheet.create(namedStyles)
   }
