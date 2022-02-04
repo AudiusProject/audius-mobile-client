3f36b234ee9caebfd9f9b2d3416c384b06677734
Dylan Jeffers <dylan@audius.co>
2022-02-04 13:09:50 -0800

    [AUD-1409] Add profile-page tracks and reposts tabs

---
+++ src/screens/profile-screen/RepostsTab.tsx
@@ -0,0 +1,48 @@
+import { useCallback } from 'react'
+
+import { feedActions } from 'audius-client/src/common/store/pages/profile/lineups/feed/actions'
+import { getProfileFeedLineup } from 'audius-client/src/common/store/pages/profile/selectors'
+import { Text } from 'react-native'
+
+import { Lineup } from 'app/components/lineup'
+import { useDispatchWeb } from 'app/hooks/useDispatchWeb'
+import { useSelectorWeb } from 'app/hooks/useSelectorWeb'
+
+import { EmptyTab } from './EmptyTab'
+import { getProfile } from './selectors'
+
+export const RepostsTab = () => {
+  const { profile } = useSelectorWeb(getProfile)
+  const lineup = useSelectorWeb(getProfileFeedLineup)
+  const dispatchWeb = useDispatchWeb()
+
+  const handlePlayTrack = useCallback(
+    (uid: string) => {
+      dispatchWeb(feedActions.play(uid))
+    },
+    [dispatchWeb]
+  )
+
+  const handlePauseTrack = useCallback(() => {
+    dispatchWeb(feedActions.pause())
+  }, [dispatchWeb])
+
+  if (!profile) return null
+
+  if (profile.repost_count === 0) {
+    return (
+      <EmptyTab>
+        <Text>{"You haven't reposted anything yet"}</Text>
+      </EmptyTab>
+    )
+  }
+
+  return (
+    <Lineup
+      actions={feedActions}
+      lineup={lineup as any}
+      playTrack={handlePlayTrack}
+      pauseTrack={handlePauseTrack}
+    />
+  )
+}
