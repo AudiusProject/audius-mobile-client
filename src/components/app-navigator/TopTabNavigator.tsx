aa0f352074a40fee9dec9184a3a5afd466a21122
Dylan Jeffers <dylan@audius.co>
2022-02-04 14:20:22 -0800

    Update screenOptions

--- src/components/app-navigator/TopTabNavigator.tsx
+++ src/components/app-navigator/TopTabNavigator.tsx
@@ -3,18 +3,16 @@ import { ComponentType, ReactNode } from 'react'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 
 import { TopTabBar } from 'app/components/top-tab-bar'
+import { makeStyles } from 'app/styles'
+import { useThemeColors } from 'app/utils/theme'
 
 const Tab = createMaterialTopTabNavigator()
 
-const screenOptions = {
-  tabBarActiveTintColor: '#CC0FE0',
-  tabBarLabelStyle: { fontSize: 12 },
-  tabBarStyle: { backgroundColor: 'white' },
-  tabBarIndicatorStyle: {
-    backgroundColor: '#CC0FE0',
-    height: 3
-  }
-}
+const useStyles = makeStyles(({ palette }) => ({
+  root: { backgroundColor: palette.white },
+  label: { fontSize: 12 },
+  indicator: { backgroundColor: palette.primary, height: 3 }
+}))
 
 type TabNavigatorProps = {
   initialScreenName?: string
@@ -25,11 +23,18 @@ export const TabNavigator = ({
   initialScreenName,
   children
 }: TabNavigatorProps) => {
+  const styles = useStyles()
+  const { primary } = useThemeColors()
   return (
     <Tab.Navigator
       initialRouteName={initialScreenName}
       tabBar={props => <TopTabBar {...props} />}
-      screenOptions={screenOptions}
+      screenOptions={{
+        tabBarActiveTintColor: primary,
+        tabBarStyle: styles.root,
+        tabBarLabelStyle: styles.label,
+        tabBarIndicatorStyle: styles.indicator
+      }}
     >
       {children}
     </Tab.Navigator>
