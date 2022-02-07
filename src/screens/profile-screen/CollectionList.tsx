da700cb557b9d9fb4238ea953b9e88e08d1517b2
Dylan Jeffers <dylan@audius.co>
2022-02-07 12:23:05 -0800

    Fix card margin, collectibles tab, import

--- src/screens/profile-screen/CollectionList.tsx
+++ src/screens/profile-screen/CollectionList.tsx
@@ -2,7 +2,7 @@ import { Collection } from 'audius-client/src/common/models/Collection'
 import { ProfileUser } from 'audius-client/src/common/store/pages/profile/types'
 import { FlatList, Text } from 'react-native'
 
-import { CollectionCard } from '../explore-screen/components/CollectionCard'
+import { CollectionCard } from 'app/screens/explore-screen/components/CollectionCard'
 
 import { EmptyTab } from './EmptyTab'
 
@@ -22,13 +22,10 @@ export const CollectionList = ({
       data={collection}
       renderItem={({ item, index }) => {
         const isInLeftColumn = !(index % 2)
-        const isHangingLastItem =
-          index === collection.length - 1 && isInLeftColumn
-
         const cardStyles = {
-          margin: 8,
+          padding: 8,
           [`margin${isInLeftColumn ? 'Left' : 'Right'}`]: 16,
-          maxWidth: isHangingLastItem ? '44%' : '50%'
+          width: '50%'
         }
 
         return (
